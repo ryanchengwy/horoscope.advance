809f67a6d4c8d5bb3301ae9b0bb261f5508fa47f
RyanC <ryan.chengwy@gmail.com>
2015-12-22 18:28:15 +0800

    Initial Commit

---
+++ horoscope.advance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  horoscope.advance
+//
+//  Created by Ryan on 22/12/2015.
+//  Copyright © 2015 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
